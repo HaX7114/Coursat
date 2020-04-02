5d6572749bd79864e1445bd90ced74261f97784c
HaX7114 <am0123238905@gmail.com>
2020-04-02 23:47:53 +0200

    Changes Added to AddAdmin.cshtml , RouteConfig.cs

--- Coursat/Controllers/MainController.cs
+++ Coursat/Controllers/MainController.cs
@@ -14,6 +14,7 @@ namespace Coursat.Controllers
             {
             System.Threading.Thread.Sleep(2000);
                 return View();
+            
             }
 
             public ActionResult Index()
