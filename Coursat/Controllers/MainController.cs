6c05c942c5731336fddae01f01708fa3e2714c0b
DodDger7 <medooortega.mx@gmail.com>
2020-04-02 23:16:36 +0200

    Retrieve password function has been added

--- Coursat/Controllers/MainController.cs
+++ Coursat/Controllers/MainController.cs
@@ -9,74 +9,74 @@ namespace Coursat.Controllers
 {
     public class MainController : Controller
     {
-            //Controllers of Views Here :
-            public ActionResult Login()
-            {
+        //Controllers of Views Here :
+        public ActionResult Login()
+        {
             System.Threading.Thread.Sleep(2000);
-                return View();
-            }
+            return View();
+        }
 
-            public ActionResult Index()
-            {
-                System.Threading.Thread.Sleep(2000);
-                return View();
-            }
+        public ActionResult Index()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return View();
+        }
 
-            public ActionResult AdminLogin()
-            {
-                System.Threading.Thread.Sleep(2000);
-                return View();
-            }
-            public ActionResult SignUp()
-            {
-                System.Threading.Thread.Sleep(2000);
-                return View();
-            }
+        public ActionResult AdminLogin()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return View();
+        }
 
-            public ActionResult SigningUp()
-            {
-                System.Threading.Thread.Sleep(2000);
-                return View();
-            }
-            public ActionResult RetrievePassword()
-            {
-                System.Threading.Thread.Sleep(2000);
-                return View();
-            }
+        public ActionResult SigningUp()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return View();
+        }
+        public ActionResult RetrievePassword()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return View();
+        }
 
 
-            public ActionResult User()
-            {
-                System.Threading.Thread.Sleep(2000);
-                return View();
-            }
+        public ActionResult User()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return View();
+        }
 
-            public ActionResult Dashboard()
-            {
-                System.Threading.Thread.Sleep(2000);
-                return View();
-            }
-            public ActionResult MyCourses()
-            {
-                System.Threading.Thread.Sleep(2000);
-                return View();
-            }
-            public ActionResult AddAdmin()
-            {
-                return View();
-            }
-            public ActionResult AddCourse()
-            {
-                return View();
-            }
-            public ActionResult EditCourse()
-            {
-                return View();
-            }
-            public ActionResult ShowCourses()
-            {
-                return View();
-            }
+        public ActionResult Dashboard()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return View();
+        }
+        public ActionResult MyCourses()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return View();
+        }
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+        public ActionResult AddCourse()
+        {
+            return View();
+        }
+        public ActionResult EditCourse()
+        {
+            return View();
+        }
+        public ActionResult ShowCourses()
+        {
+            return View();
+        }
 
 
 
@@ -96,10 +96,10 @@ namespace Coursat.Controllers
         // User Login Function :
         [HttpPost]
         public ActionResult user(WEBSITE_USER user)
-            {
+        {
             try
             {
-                
+
                 if (user.Email == null || user.Password == null)
                 {
                     System.Threading.Thread.Sleep(2000);
@@ -129,27 +129,27 @@ namespace Coursat.Controllers
                         return View("Login");
 
                     }
-            
-
-                    }
-                }
-            catch (Exception e)
-                {
-
-                        ViewBag.Message = false;
-                        return View("Login");
 
 
                 }
-        
             }
+            catch (Exception e)
+            {
+
+                ViewBag.Message = false;
+                return View("Login");
+
+
+            }
+
+        }
 
         [HttpPost]
         public ActionResult AdminDashboard(WEBSITE_USER admin)
-            {
+        {
             try
             {
-                
+
                 if (admin.Email == null || admin.Password == null)
                 {
                     System.Threading.Thread.Sleep(2000);
@@ -190,78 +190,127 @@ namespace Coursat.Controllers
 
             }
 
-            }
+        }
 
         [HttpPost]
         public ActionResult FinishingUp(USER user)
+        {
+
+            try
             {
-                
-                try
+
+                if (user.Password == null || user.RepeatPass == null || user.Email == null || user.User_name == null)
                 {
-                    
-                    if (user.Password == null || user.RepeatPass == null || user.Email == null || user.User_name == null)
+                    System.Threading.Thread.Sleep(2000);
+                    ViewBag.Empty = true;
+                    return View("SignUp");
+
+                }
+                else if (user.Password != user.RepeatPass)
+                {
+                    System.Threading.Thread.Sleep(2000);
+                    ViewBag.Pass = false;
+                    return View("SignUp");
+
+                }
+                else
+                {
+                    String User_Email = new DB_CONNECTION().Database.SqlQuery<String>("SELECT Email FROM USERS WHERE Email ='" + user.Email + "'").FirstOrDefault<String>();
+
+                    String User_name = new DB_CONNECTION().Database.SqlQuery<String>("SELECT User_name FROM USERS WHERE User_name ='" + user.User_name + "'").FirstOrDefault<String>();
+
+                    if (User_Email != null)
                     {
                         System.Threading.Thread.Sleep(2000);
-                        ViewBag.Empty = true;
+                        ViewBag.RepeatedEmail = true;
                         return View("SignUp");
-
                     }
-                    else if (user.Password != user.RepeatPass)
+                    else if (User_name != null)
                     {
                         System.Threading.Thread.Sleep(2000);
-                        ViewBag.Pass = false;
+                        ViewBag.RepeatedUser = true;
                         return View("SignUp");
-
                     }
                     else
                     {
-                        String User_Email = new DB_CONNECTION().Database.SqlQuery<String>("SELECT Email FROM USERS WHERE Email ='" + user.Email + "'").FirstOrDefault<String>();
-
-                        String User_name = new DB_CONNECTION().Database.SqlQuery<String>("SELECT User_name FROM USERS WHERE User_name ='" + user.User_name + "'").FirstOrDefault<String>();
-
-                        if(User_Email != null)
-                        {
-                            System.Threading.Thread.Sleep(2000);
-                            ViewBag.RepeatedEmail = true;
-                            return View("SignUp");
-                        }
-                        else if(User_name != null)
-                        {
-                            System.Threading.Thread.Sleep(2000);
-                            ViewBag.RepeatedUser = true;
-                            return View("SignUp");
-                        }
-                        else
-                        {
-                            System.Threading.Thread.Sleep(2000);
-                            new DB_CONNECTION().Database.ExecuteSqlCommand
-                                   ("INSERT INTO USERS (Email , User_name , Password) VALUES ('" + user.Email + "'," + "'" + user.User_name + "'," + "'" + user.Password + "')");
-                            return View("SigningUp");
-                        }
-                        
-                       
-                    
+                        System.Threading.Thread.Sleep(2000);
+                        new DB_CONNECTION().Database.ExecuteSqlCommand
+                               ("INSERT INTO USERS (Email , User_name , Password) VALUES ('" + user.Email + "'," + "'" + user.User_name + "'," + "'" + user.Password + "')");
+                        return View("SigningUp");
                     }
-
 
 
 
                 }
-                catch(Exception e)
-                    {
-                        System.Threading.Thread.Sleep(2000);
-                        new DB_CONNECTION().Database.ExecuteSqlCommand
-                                   ("INSERT INTO USERS (Email , User_name , Password) VALUES ('" + user.Email + "'," + "'" + user.User_name + "'," + "'" + user.Password + "')");
-                        return View("SigningUp");
-                    }
+
+
+
 
             }
+            catch (Exception e)
+            {
+                System.Threading.Thread.Sleep(2000);
+                new DB_CONNECTION().Database.ExecuteSqlCommand
+                           ("INSERT INTO USERS (Email , User_name , Password) VALUES ('" + user.Email + "'," + "'" + user.User_name + "'," + "'" + user.Password + "')");
+                return View("SigningUp");
+            }
+
+        }
 
         [HttpPost]
         public ActionResult Retrievepassword(USER user)  //Dodger's function
         {
-            return View("RetrievePassword");
+            try
+            {
+
+                if (user.Email == null || user.User_name == null)
+                {
+                    System.Threading.Thread.Sleep(2000);
+                    ViewBag.Empty = true;
+                    return View("RetrievePassword");
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(2000);
+
+                    String User_Email = new DB_CONNECTION().Database.SqlQuery<String>("SELECT Email FROM USERS WHERE Email ='" + user.Email + "'").FirstOrDefault<String>();
+
+                    String User_name = new DB_CONNECTION().Database.SqlQuery<String>("SELECT User_name FROM USERS WHERE User_name ='" + user.User_name + "'").FirstOrDefault<String>();
+
+
+                    if (user.Email.Equals(User_Email) && user.User_name.Equals(User_name))
+                    {
+                        String Password = new DB_CONNECTION().Database.SqlQuery<String>("SELECT Password FROM USERS WHERE Email ='" + user.Email + "'").FirstOrDefault<String>();
+                        ViewBag.Password = Password;
+                        ViewBag.Found = true;
+                        System.Threading.Thread.Sleep(2000);
+
+                        return View("RetrievePassword");
+
+                    }
+                    else
+                    {
+
+                        ViewBag.isRight = false;
+                        return View("RetrievePassword");
+
+                    }
+
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                ViewBag.isRight = false;
+                return View("RetrievePassword");
+
+
+            }
+
         }
+
+
 
         [HttpPost]
         public ActionResult Addadmin(ADMIN admin)  //Kaboria's function
